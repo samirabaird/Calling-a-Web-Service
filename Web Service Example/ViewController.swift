72879d3244e2285c7f3e71a675d63f9777aabf84
Your Name <sbaird@sch.org>
2016-01-09 11:35:40 -0500

    Initial Commit

---
+++ Web Service Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example
+//
+//  Created by Samira Baird on 1/9/16.
+//  Copyright © 2016 Samira Baird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
